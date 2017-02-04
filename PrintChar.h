a158bcd55bd304c535051c5c269b8d111835e97e
Jack Kou <jackouzz@hotmail.com>
2017-02-04 21:56:39 +0800

    C language

---
+++ PrintChar.h
@@ -0,0 +1,19 @@
+int PrintChar ( char * string )
+{
+	//int i;
+	char temp;
+	temp = *string;
+	
+	if (temp == '\0') 
+	{
+		return (-1);
+	}
+	while (temp != '\0')
+	{
+		printf ("%c /n",temp);
+		string++;
+		temp = *string;
+	}
+	return(0);
+		
+}
---
+++ main.c
@@ -0,0 +1,22 @@
+//#include <PrintChar.h>
+int main()
+{
+  //char Mystring[9];
+  int temp;
+  //for (temp=0;temp<9;temp++)
+   // {
+     // Mystring[temp] = 'A';
+     // 
+   // }
+	//temp = PrintChar ( & Mystring );
+	temp =  0;
+	if (temp == 0) 
+	{
+		printf ("%d %d \n",111,temp);
+			
+	}
+	if (temp == -1)
+	{
+		printf ("%s \n","The string is Null");
+	}
+}
