58df1cb3621dc5d962b9c1e97879c6688d1edea2
Jack Kou <jackouzz@hotmail.com>
2017-10-03 14:54:55 +0800

    改造练习代码

--- learning.py
+++ learning.py
@@ -1,6 +1,20 @@
 #!/user/bin/env python3
 # -*- coding: utf-8 -*-
-print ('Hello World!')
-#name = ''
-name = input ('请输入你的名字：')
-print ('Hello ', name ,'!')
+
+def triangles():
+
+    return
+
+def Display():
+    print('Hello World!')
+    name = input('请输入你的名字：')
+    print('Hello', name, '!')
+    return
+
+def main():
+    Display()
+
+    return
+
+main()
+
