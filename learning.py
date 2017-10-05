6c7c215fded16b508fcf8fc4a8e0009be61cbc11
Jack Kou <jackouzz@hotmail.com>
2017-10-05 16:14:42 +0800

    判断回数

--- learning.py
+++ learning.py
@@ -3,17 +3,29 @@
 
 from functools import reduce
 
+#判断是否回数
+def is_palindrome(n):
+    if n < 10 :
+        return False
+    L=str(n)
+    i=0
+    for i in range(int(len(L)/2)):
+        if L[i] != L[len(L)-i-1]:
+            return False
+    return True
+
 #map，reduce混合用法示范
 def str2float(s):
     L1=s[:s.index('.')]
     L2=s[s.index('.')+1:]
     L2=L2[::-1]
-    print(L1)
-    print(L2)
+
     def fn(x, y):
         return x * 10 + y
+
     def char2num(k):
         return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[k]
+
     def fm(x,y):
         return x * 0.1 + y
     return reduce(fn,map(char2num,L1)) + reduce(fm,map(char2num,L2))*0.1
@@ -29,7 +41,7 @@ def normalize(name):
     return name.capitalize()
 
 #演示map()用法，将一组英文字符串全部改为首字母大写，其余字母小写
-def ShowMap():
+def showMap():
     L = ['adam', 'LISA', 'barT']
     L2 = list(map(normalize, L))
     print(L2)
@@ -44,10 +56,9 @@ def triangles():
         yield L
         L.append(0)
         L = [L[i - 1] + L[i] for i in range(len(L))]
-    return
 
 #调用杨辉三角形函数示范
-def Showtriangles():
+def showtriangles():
     n = 0
     for g in triangles():
         print (g)
@@ -56,20 +67,21 @@ def Showtriangles():
             break
     return
 
-def Display():
+def display():
     print('Hello World!')
     name = input('请输入你的名字：')
     print('Hello', name, '!')
     return
 
 def main():
-    #Display()
-    #Showtriangles()
-    #ShowMap()
+    #display()
+    #showtriangles()
+    #showMap()
     #print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
-    print('str2float(\'123.456\') =', str2float('123.456'))
+    #print('str2float(\'123.456\') =', str2float('123.456'))
+    output = filter(is_palindrome, range(9999, 99999))
+    print(list(output))
 
     return
 
 main()
-
